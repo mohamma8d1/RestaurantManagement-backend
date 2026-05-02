701f637a7d5059d1092f44db51a22126f6a04eb7
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-02 23:55:59 +0330

    Create Reservation Enum

---
+++ RestaurantManagement.Domain/Enums/ReservationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Enums;
+
+public enum ReservationStatus
+{
+    Pending = 0,
+    Confirmed = 1,
+    Completed = 2,
+    Cancelled = 3,
+}
