f43f5d4da3704b88bf38dae46da0597a6eecd218
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-03 00:01:18 +0330

    Create User Entity

---
+++ RestaurantManagement.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string FullName {get; set;} = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "Customer";
+    public bool IsActive { get; set; }
+
+
+}
