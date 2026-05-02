42e3eb6fbcdb9d88a1392a474831e201925b8b51
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-02 23:54:12 +0330

    Create OrderStatus Enums

---
+++ RestaurantManagement.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Enums;
+
+public enum OrderStatus
+{
+    Pending = 0,
+    Confirmed = 1,
+    Preparing = 2,
+    Ready = 3,
+    Delivered = 4,
+    Cancelled = 5,
+}
