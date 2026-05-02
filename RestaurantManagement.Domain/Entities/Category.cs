c191eecff3048f0b9771c44a6b9265a708fd52b4
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-03 00:16:25 +0330

    Create Catrgory Entity

---
+++ RestaurantManagement.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
+    public virtual ICollection<FoodItem> FoodItem { get; set; } = new List<FoodItem>();
+
+}
