c0782c6ad51baa6944456dd1e62a1a71b57d7aac
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-03 00:35:07 +0330

    Create Order entity

---
+++ RestaurantManagement.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class Order : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public decimal TotalAmount { get; set; }
+    public OrderStatus status { get; set; } = OrderStatus.Pending;
+    public string? OrderTransactionId { get; set; }
+
+    public virtual User User { get; set; } = null!;
+
+}
