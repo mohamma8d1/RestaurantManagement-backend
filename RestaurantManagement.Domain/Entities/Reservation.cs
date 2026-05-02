cfec355a8344a5ae156295f5c3519774b0fa96e3
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-03 00:54:00 +0330

    Complete Reservation Entity

---
+++ RestaurantManagement.Domain/Entities/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantManagement.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class Reservation : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int NumberOfGuests { get; set; }
+    public string? SpecialRequests { get; set; }
+    public ReservationStatus Status { get; set; } = ReservationStatus.Pending;
+
+    public virtual User User { get; set; } = null!;
+}
