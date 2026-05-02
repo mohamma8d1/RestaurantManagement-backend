f0aa3ff32aee10171cf8be53a6df886f944443c1
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-02 23:50:46 +0330

    create Base Entity

---
+++ RestaurantManagement.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class BaseEntity
+{
+    public Guid id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdateTime { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
