0805904ce79bb444f4d8177e829b4d9ff2d4f9f7
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-03 00:13:03 +0330

    Create Food Item Entity

---
+++ RestaurantManagement.Domain/Entities/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Domain.Entities;
+
+public class FoodItem : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public Guid CategoryId { get; set; }
+    public bool IsAvailable { get; set; } = true;
+}
