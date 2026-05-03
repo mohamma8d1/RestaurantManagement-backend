986a08086bba1483f4097652da99c4fd3db41007
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-04 01:43:16 +0330

    Create & complete Infrastructure DI

---
+++ RestaurantManagement.Infrastructure/DependencyInjection/InfrastructureServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantManagement.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Infrastructure.DependencyInjection;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("Defualt")));
+
+        return services;
+    }
+}
