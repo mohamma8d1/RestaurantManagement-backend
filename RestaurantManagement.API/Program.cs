aeaa2e55c20e0b63a533eb40baa54619183e4c92
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-04 01:44:56 +0330

    add infrastuvture service in program.cs

--- RestaurantManagement.API/Program.cs
+++ RestaurantManagement.API/Program.cs
@@ -1,6 +1,8 @@
+using RestaurantManagement.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddControllers();
 
