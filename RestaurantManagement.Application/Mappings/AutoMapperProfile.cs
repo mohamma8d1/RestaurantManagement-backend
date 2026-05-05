2b9ad5b0ae828be3083f4f3efd952b6f19bba2fb
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-05 23:59:54 +0330

    set AutoMapper

---
+++ RestaurantManagement.Application/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using RestaurantManagement.Application.DTOs.Auth;
+using RestaurantManagement.Domain.Entities;
+
+namespace RestaurantManagement.Application.Mappings;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<RegisterDto, User>().ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
+    }
+}
