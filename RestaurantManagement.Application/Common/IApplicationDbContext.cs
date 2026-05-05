2b5886f62d526822d3e9976d8cdab3fc1d88c97a
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-06 00:27:05 +0330

    create interface appDb Context

---
+++ RestaurantManagement.Application/Common/IApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RestaurantManagement.Application.Common;
+
+public interface IApplicationDbContext
+
+{
+    IQueryable<User> Users { get; }
+    IQueryable<Category> Categories { get; }
+    IQueryable<FoodItem> FoodItems { get; }
+    IQueryable<Order> Orders { get; }
+    IQueryable<OrderItem> OrderItems { get; }
+    IQueryable<Reservation> Reservations { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
