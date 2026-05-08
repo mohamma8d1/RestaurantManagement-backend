4719d33b48e629ed59a7889504fce66e59e54dea
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-08 14:59:44 +0330

    create IUserRepository

---
+++ RestaurantManagement.Application/Common/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Common.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+    Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken);
+    Task AddAsync(User user, CancellationToken cancellationToken);
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
