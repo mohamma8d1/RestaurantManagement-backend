2f746ad2a318dfa278554d84f78d48695cd40665
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-08 15:10:26 +0330

    complete User Repository

---
+++ RestaurantManagement.Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagement.Application.Common.Interfaces;
+using RestaurantManagement.Domain.Entities;
+
+namespace RestaurantManagement.Infrastructure.Data.Repositories;
+
+public class UserRepository(AppDbContext context) : IUserRepository
+{
+    public async Task AddAsync(User user, CancellationToken cancellationToken)
+    {
+        await context.AddAsync(user, cancellationToken);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken)
+    {
+        return await context.Users.FirstOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public async Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken)
+    {
+        return !await context.Users.AnyAsync(u  => u.Email == email, cancellationToken);
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
