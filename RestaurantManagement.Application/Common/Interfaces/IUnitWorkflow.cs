c03b9f9ecec299784b9acec1ba5bd97169d5a1b4
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-08 15:12:52 +0330

    create interface unit workflow

---
+++ RestaurantManagement.Application/Common/Interfaces/IUnitWorkflow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Common.Interfaces;
+
+public interface IUnitWorkflow
+{
+    IUserRepository Users { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
