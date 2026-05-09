40a3aa49dfbe489ff065438e7927b6023208f20e
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-10 02:21:53 +0330

    add register controller

---
+++ RestaurantManagement.API/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantManagement.Application.DTOs.Auth;
+using RestaurantManagement.Application.Features.Auth.Commands.Register;
+
+namespace RestaurantManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var result = await mediator.Send(new RegisterUserCommand(registerDto));
+            return Ok(result);
+        }
+    }
+}
