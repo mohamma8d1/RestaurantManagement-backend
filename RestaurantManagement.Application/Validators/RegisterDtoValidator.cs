e526dd2e1c88a4cc71ddc85ae5e146770196113d
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-11 23:13:14 +0330

    some cchanges in messege validator

--- RestaurantManagement.Application/Validators/RegisterDtoValidator.cs
+++ RestaurantManagement.Application/Validators/RegisterDtoValidator.cs
@@ -13,7 +13,7 @@ public class RegisterDtoValidator : AbstractValidator<RegisterDto>
     public RegisterDtoValidator()
     {
         RuleFor(x => x.FullName).NotEmpty().WithMessage("Full name is Required").MinimumLength(6).WithMessage("Full name must be at least 6 character");
-        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is Required").EmailAddress().WithMessage("Email address invalid");
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is Required").EmailAddress().WithMessage("Email address format invalid");
         RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("Phone number is Required").Matches(@"^09[0-9]{9}$").WithMessage("Invalid phone number format");
         RuleFor(x => x.Password).NotEmpty().WithMessage("Password is Required").MinimumLength(8).WithMessage("Password must be at least 8 character");
         RuleFor(x => x.Role).Must(role => new[] { "Customer", "Admin", "Chef", "Waiter"  }.Contains(role)).WithMessage("Role must be Customer, Admin, Chef, or Waiter");
