b4ed11fbe428fd0da86118b5b8bdb3b2d2ca81c9
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-11 23:12:39 +0330

    add Login dto validator

---
+++ RestaurantManagement.Application/Validators/LoginDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using RestaurantManagement.Application.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Validators;
+
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is Required").EmailAddress().WithMessage("Email address invalid format");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password is Required");
+    }
+}
