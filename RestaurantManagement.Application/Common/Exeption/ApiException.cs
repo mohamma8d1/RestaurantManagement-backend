3bde9098dcca32b4590225b694f98d42325446b1
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-13 00:04:17 +0330

    Add Exeption Handler for Integrate messages

---
+++ RestaurantManagement.Application/Common/Exeption/ApiException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Common.Exeption;
+
+public class ApiException : Exception
+{
+    public int StatusCode { get; set; }
+
+    public ApiException(string message, int statusCode = 400) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
