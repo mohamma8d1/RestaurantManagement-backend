b9de3e353ac9ad37856850604597e294f742c42b
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-12 23:52:41 +0330

    add application service

---
+++ RestaurantManagement.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace RestaurantManagement.Application;
+
+public static class ApplicationServicesRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+}
