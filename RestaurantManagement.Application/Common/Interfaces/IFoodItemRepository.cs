62b4fded325ee65a3f85a055464c6e549c1eedb1
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-14 03:01:39 +0330

    add FoodItem repo interface

---
+++ RestaurantManagement.Application/Common/Interfaces/IFoodItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantManagement.Domain.Entities;
+using System;
+
+namespace RestaurantManagement.Application.Common.Interfaces;
+
+public interface IFoodItemRepository
+{
+    Task<FoodItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<IReadOnlyList<FoodItem>> GetAllAsync(CancellationToken cancellationToken);
+    Task<IReadOnlyList<FoodItem>> GetByCategoryAsync(Guid categoryId, CancellationToken cancellationToken);
+    Task AddAsync(FoodItem foodItem, CancellationToken cancellationToken);
+    void Update(FoodItem foodItem);
+    void Delete(FoodItem foodItem);
+}
