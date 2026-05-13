541e5ae27077b520820f6832135f2ffb099fdd81
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-14 03:22:24 +0330

    update unit work flow

--- RestaurantManagement.Infrastructure/Data/UnitOfWork.cs
+++ RestaurantManagement.Infrastructure/Data/UnitOfWork.cs
@@ -7,7 +7,14 @@ namespace RestaurantManagement.Infrastructure.Data;
 public class UnitOfWork(AppDbContext context) : IUnitOfWork
 {
     private IUserRepository? userRepository;
+    private ICategoryRepository? categoryRepository;
+    private IFoodItemRepository? foodItemRepository;
+
     public IUserRepository Users => userRepository ??= new UserRepository(context);
+
+    public ICategoryRepository Category => categoryRepository ??= new CategoryRepository(context);
+
+    public IFoodItemRepository FoodItem => foodItemRepository ??= new FoodItemRepository(context);
 
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
     {
