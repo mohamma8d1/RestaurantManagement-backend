d1c164e50a0b017c0d332ff64b3b11af08674860
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-14 03:03:23 +0330

    add this two repo to unitworkflow interface

--- RestaurantManagement.Application/Common/Interfaces/IUnitOfWork.cs
+++ RestaurantManagement.Application/Common/Interfaces/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace RestaurantManagement.Application.Common.Interfaces;
 public interface IUnitOfWork
 {
     IUserRepository Users { get; }
-
+    ICategoryRepository Category { get; }
+    IFoodItemRepository FoodItem { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
