80af4f7f9f09b3d6907637ba9f00ea7ac0775d2c
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-15 23:43:59 +0330

    create Category Dto

---
+++ RestaurantManagement.Application/DTOs/Menu/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.DTOs.Menu;
+
+public class CategoryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
