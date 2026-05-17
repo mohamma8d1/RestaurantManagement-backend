d4cec270ebfaf523c1940f6693b9c1808e14db6e
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-17 22:37:13 +0330

    config AutoMapper to New Dtos

--- RestaurantManagement.Application/Mappings/AutoMapperProfile.cs
+++ RestaurantManagement.Application/Mappings/AutoMapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using AutoMapper;
 using RestaurantManagement.Application.DTOs.Auth;
+using RestaurantManagement.Application.DTOs.Menu;
 using RestaurantManagement.Domain.Entities;
 
 namespace RestaurantManagement.Application.Mappings;
@@ -12,5 +13,10 @@ public class AutoMapperProfile : Profile
         CreateMap<RegisterDto, User>().ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
             .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
             .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
+        CreateMap<Category, CategoryDto>();
+        CreateMap<FoodItem, FoodItemDto>().ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+        CreateMap<CreateFoodItemDto, FoodItem>();
+        CreateMap<UpdateFoodItemDto, FoodItem>();
+
     }
 }
