a5382d63c7bbe0ff5c66fc71f37c2300558b2bb2
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-17 22:30:12 +0330

    create CreateFoodItemDto

---
+++ RestaurantManagement.Application/DTOs/Menu/CreateFoodItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.DTOs.Menu;
+
+public class CreateFoodItemDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public Guid CategoryId { get; set; }
+    public bool IsAvailable { get; set; }
+
+}
