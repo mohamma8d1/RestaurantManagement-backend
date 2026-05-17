75554f8d94e53b7a070b56d7e81b9648b10f2424
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-17 22:51:43 +0330

    complete food item Query and handler

---
+++ RestaurantManagement.Application/Features/Menu/Queries/FoodItems/FoodItemsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using RestaurantManagement.Application.DTOs.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Application.Features.Menu.Queries.FoodItems;
+
+public record FoodItemsQuery : IRequest<IReadOnlyList<FoodItemDto>>;
+
---
+++ RestaurantManagement.Application/Features/Menu/Queries/FoodItems/FoodItemsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MediatR;
+using RestaurantManagement.Application.Common.Interfaces;
+using RestaurantManagement.Application.DTOs.Menu;
+using System;
+
+namespace RestaurantManagement.Application.Features.Menu.Queries.FoodItems;
+
+public class FoodItemsQueryHandler(IUnitOfWork unitWork, IMapper mapper) : IRequestHandler<FoodItemsQuery, IReadOnlyList<FoodItemDto>>
+{
+    public async Task<IReadOnlyList<FoodItemDto>> Handle(FoodItemsQuery request, CancellationToken cancellationToken)
+    {
+        var foodItems = await unitWork.FoodItem.GetAllAsync(cancellationToken);
+        return mapper.Map<IReadOnlyList<FoodItemDto>>(foodItems);
+    }
+}
