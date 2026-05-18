ddcf16263480bf6e7e23e4e2f36c10c3ffbee9e1
mohamma8d1 <mmmirnoori@gmail.com>
2026-05-18 23:26:22 +0330

    create food item controller ( not completed )

---
+++ RestaurantManagement.API/Controllers/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantManagement.Application.DTOs.Menu;
+using RestaurantManagement.Application.Features.Menu.Command.FoodItems;
+using RestaurantManagement.Application.Features.Menu.Queries.FoodItems;
+
+namespace RestaurantManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MenuController(IMediator mediator) : ControllerBase
+{
+
+    [HttpGet("fooditems")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetAllFoodItems()
+    {
+        var result = await mediator.Send(new GetFoodItemsQuery());
+        return Ok(result);
+    }
+
+    [HttpPost("fooditems")]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateFoodItem(CreateFoodItemDto dto)
+    {
+        var result = await mediator.Send(new CreateFoodItemCommand(dto));
+        return Ok(result);
+    }
+
+
+}
