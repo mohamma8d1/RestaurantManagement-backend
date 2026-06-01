6156ace09cfa3d6c7b984cfaad082234e330b4b2
MohammadReza Mirnoori <100116858+mohamma8d1@users.noreply.github.com>
2026-06-01 13:25:14 +0330

    make it correct

--- Tests/RestaurantManagement.Tests.cs
+++ Tests/RestaurantManagement.Tests.cs
@@ -83,7 +83,7 @@ public class LoginUserCommandHandlerTests
             .Setup(r => r.GetByEmailAsync(user.Email, It.IsAny<CancellationToken>()))
             .ReturnsAsync(user);
 
-        _jwtService.Setup(j => j.GenerateToken(user)).Returns("wrong-token");
+        _jwtService.Setup(j => j.GenerateToken(user)).Returns("mock-token");
         _jwtService.Setup(j => j.GenerateRefreshToken()).Returns("mock-refresh");
 
         var command = new LoginUserCommand(new LoginDto
